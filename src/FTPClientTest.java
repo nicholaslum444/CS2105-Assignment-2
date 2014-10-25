45a26ea4a1d4eb95e569c10ae51c232588951c1d
Nicholas Lum <nicholaslum444@hotmail.com>
2014-10-25 22:17:06 +0800

    etsting
    Signed-off-by: Nicholas Lum <nicholaslum444@hotmail.com>

---
+++ src/FTPClientTest.java
@@ -0,0 +1,20 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class FTPClientTest {
+
+	@Test
+	public void test() {
+		assertEquals(1, 1);
+		//fail("Not yet implemented");
+	}
+	
+	@Test
+	public void tes1t() {
+		assertEquals(1, 1);
+		fail("Not yet implemented");
+	}
+
+}
